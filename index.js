73a859c03671a00a76e5396d882f4ae196add6a2
Tishasoumya-02 <tishasoumya@gmail.com>
2021-03-15 12:23:12 +0530

    Version 1.0 2021-03-15

---
+++ index.js
@@ -0,0 +1,124 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const ejs=require('ejs');
+const fetch=require("node-fetch");
+require('dotenv').config();
+const response = require("express");
+const app=express();
+
+ 
+
+ 
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.get("/",function(req,res)
+{
+    res.sendFile(__dirname+ "/index.html");
+});
+const myAPI_KEY=(process.env.API_KEY);
+const url="https://api.nasa.gov/planetary/apod?api_key=";
+const curl=url+myAPI_KEY;
+
+app.get("/addendum", function(req,res)
+{
+   
+
+       fetch(curl)
+      .then(response=>response.json())
+      .then(data=>{
+        
+       
+        
+        res.render('C:/Users/HP/desktop/upspace1/public/views/index',{info : {
+            body: data.explanation,
+            imgurl : data.url, 
+            heading: data.title,
+            date :data.date
+        }
+            
+        });
+        
+      }).catch(error=>{
+          console.log(error);
+      });
+   
+   
+});
+app.get("/celestial",function(req,res)
+{
+    res.sendFile(__dirname+ "/public/celestial.html");
+    
+});
+app.get("/icon",function(req,res)
+{
+    res.sendFile(__dirname+ "/index.html");
+    
+});
+app.get("/celestial/fireball",function(req,res)
+{
+    var i;
+    fetch("https://ssd-api.jpl.nasa.gov/fireball.api?limit=60")
+    .then(response=>response.json())
+    .then(data=>{
+        var data1=[];
+          for(i=0;i<data.data.length;i++)
+           {
+              data1.push(data.data[i]);
+           }
+           res.render('C:/Users/HP/desktop/upspace1/public/views/index1',{set :data1});
+           
+    }).catch(error=>{
+        console.log(error);
+    });
+
+
+});
+app.get("/celestial/cad",function(req,res)
+{
+    var i;
+    fetch("https://ssd-api.jpl.nasa.gov/cad.api?dist-max=10LD&date-min=2018-01-01&sort=dist&limit=60&body=ALL&fullname=true")
+    .then(response=>response.json())
+    .then(data=>{
+        var data1=[];
+          for(i=0;i<data.data.length;i++)
+           {
+              data1.push(data.data[i]);
+           }
+           res.render('C:/Users/HP/desktop/upspace1/public/views/index2',{set :data1});
+           
+    }).catch(error=>{
+        console.log(error);
+    });
+
+
+});
+
+
+
+
+
+
+app.listen(3000,function()
+{
+    console.log("Server is running on port-3000");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ public/Javascript/site.js
@@ -0,0 +1,23 @@
+const texts=['Intergalactic','Sidereal ','Interstellar','Extraterrestrial']; 
+let count=0;
+let index=0;
+let currentText=' ';
+let letter= '';
+(function type()
+{
+if(count===texts.length)
+{
+   
+    count=0;
+}
+currentText=texts[count];
+letter=currentText.slice(0,++index);
+document.querySelector('.typing').innerHTML=letter;
+if(letter.length===currentText.length)
+{
+    count++;
+    index=0;
+}
+setTimeout(type,400);
+
+}());
